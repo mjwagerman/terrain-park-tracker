1bcd0c12ff46b23f6a915273b7d1bec123bed146
Mathew <mathew@Mathews-Laptop.attlocal.net>
2024-11-11 14:44:19 -0800

    starting styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import Auth from './Auth';
+import Auth from './Auth'; 
 
 const Header = () => {
   
     return (
       <div className = "header">
+      <img src="./src/assets/mappin.png" alt="logo" className="logo-img"></img>
         <h1>TerrainParkTracker</h1>
         <Auth />
       </div>
