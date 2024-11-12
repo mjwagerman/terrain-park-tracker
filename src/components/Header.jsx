d4966cbd9287ed1f6ed10f7400484a1fba73562c
mjwagerman <mjwagerman@gmail.com>
2024-11-12 07:45:52 -0800

    more css, header links:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,18 @@ const Header = () => {
     return (
       <div className = "header">
 
-        <Link to="/"><h1>TerrainParkTracker</h1> </Link>
-      <img src="./src/assets/mappin.png" alt="logo" className="logo-img"></img>
-        <h1>TerrainParkTracker</h1>
+        <Link to="/" className="header-link-home">
+          <img src="./src/assets/mappin.png" alt="logo" className="logo-img"></img>
+          <h1>TerrainParkTracker</h1> 
+        </Link>
+        <div className="header-list-links">
+          <Link to="/resorts" className="header-link-resorts">
+            Resorts
+          </Link>
+          <Link to="/about" className="header-link-about">
+            About
+          </Link>
+        </div>
         <Auth />
       </div>
     )
