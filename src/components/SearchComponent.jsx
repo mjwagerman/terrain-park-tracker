bebf3f988f492d3832d0e62537bb1b467074e0f0
mjwagerman <mjwagerman@gmail.com>
2024-11-12 13:12:49 -0800

    added search function

---
+++ src/components/SearchComponent.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState,useEffect } from "react";
+
+import { getResorts } from "../services/getResorts";
+
+
+const getFilteredResorts = (query, resorts) => {
+    if (!query) {
+        return resorts;
+    }
+    return resorts.filter((resort) => resort.name.toLowerCase().includes(query.toLowerCase()));
+}
+
+export default function SearchComponent() {
+
+    const [query, setQuery] = useState("");
+    
+
+    const[resortList, setResortList] = useState([]);  
+    useEffect(() => {
+        const fetchResorts = async() => {
+          try {
+            const resorts = await getResorts();
+            setResortList(resorts);
+          } catch (err) {
+            console.error(err)
+          }
+        };
+        fetchResorts();
+      }, []);
+
+      const filteredResorts = getFilteredResorts(query, resortList);
+    
+
+    
+    return (
+        <div>
+            <input type="text" onChange={e=> setQuery (e.target.value)} />
+            <ul>
+                {filteredResorts.map(value => <h1 key={value.name}>{value.name}</h1>)}
+            </ul>
+        </div>
+    )
+}
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Header from '../components/Header';
 import MapComponent from '../components/MapComponent';
 import InputResort from '../components/InputResort';
+import SearchComponent from '../components/SearchComponent';
 
 export default function HomeScreen() {
   return (
     <div>
         <MapComponent />
         <InputResort />
+        <SearchComponent />
     </div>
   )
 }
